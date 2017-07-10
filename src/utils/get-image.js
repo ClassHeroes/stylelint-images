4f9cbcb164f61d122125ebf5dd34940ddf0f4049
Eric Heaton <eric.heaton@b-reel.com>
2017-07-10 11:33:15 -0400

    feat(get-image): Support for checking local images

--- src/utils/get-image.js
+++ src/utils/get-image.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
 
+const path = require('path');
+const fs = require('fs');
+
 const cacheImageResults = new Map();
 
-export default function getImage(url) {
+// Remote URLs
+// From: http://stackoverflow.com/a/19709846/4709891
+const REMOTE_URL = /^(?:[a-z]+:)?\/\//i;
+
+export default function getImage(url, source) {
+  let fileExistsCheck = null;
+  let testPath = null;
+
   if (cacheImageResults.has(url)) {
     return preservePromiseMethods(cacheImageResults.get(url));
   }
 
-  return axios.get(url, { responseType: 'arraybuffer' })
+  if (!url.match(REMOTE_URL)) {
+    testPath = path.join(path.dirname(source), url);
+    fileExistsCheck = localFileExists;
+  } else {
+    testPath = url;
+    fileExistsCheck = remoteFileExists;
+  }
+
+  return fileExistsCheck(testPath)
     .then((response) => {
       cacheImageResults.set(url, response);
       return response;
@@ -25,4 +43,21 @@ function preservePromiseMethods(result) {
   }
 
   return Promise.resolve(result);
+}
+
+function remoteFileExists(fileURL) {
+  return axios.get(fileURL, { responseType: 'arraybuffer' });
+}
+
+function localFileExists(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filePath, (error, stat) => {
+      if (error) {
+        error.response = { status: 404 };
+        reject(error);
+      } else {
+        resolve(stat);
+      }
+    });
+  });
 }
