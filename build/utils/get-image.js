e7181ffb752425e5c76b3eedca71da040f8eeb4c
Eric Heaton <eric.heaton@b-reel.com>
2017-07-10 13:04:43 -0400

    fix(get-image): Updated build

--- build/utils/get-image.js
+++ build/utils/get-image.js
@@ -11,14 +11,32 @@ var _axios2 = _interopRequireDefault(_axios);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var path = require('path');
+var fs = require('fs');
+
 var cacheImageResults = new Map();
 
-function getImage(url) {
+// Remote URLs
+// From: http://stackoverflow.com/a/19709846/4709891
+var REMOTE_URL = /^(?:[a-z]+:)?\/\//i;
+
+function getImage(url, source) {
+  var fileExistsCheck = null;
+  var testPath = null;
+
   if (cacheImageResults.has(url)) {
     return preservePromiseMethods(cacheImageResults.get(url));
   }
 
-  return _axios2.default.get(url, { responseType: 'arraybuffer' }).then(function (response) {
+  if (!url.match(REMOTE_URL)) {
+    testPath = path.join(path.dirname(source), url);
+    fileExistsCheck = localFileExists;
+  } else {
+    testPath = url;
+    fileExistsCheck = remoteFileExists;
+  }
+
+  return fileExistsCheck(testPath).then(function (response) {
     cacheImageResults.set(url, response);
     return response;
   }).catch(function (error) {
@@ -33,5 +51,22 @@ function preservePromiseMethods(result) {
   }
 
   return Promise.resolve(result);
+}
+
+function remoteFileExists(fileURL) {
+  return _axios2.default.get(fileURL, { responseType: 'arraybuffer' });
+}
+
+function localFileExists(filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.stat(filePath, function (error, stat) {
+      if (error) {
+        error.response = { status: 404 };
+        reject(error);
+      } else {
+        resolve(stat);
+      }
+    });
+  });
 }
 //# sourceMappingURL=get-image.js.map
